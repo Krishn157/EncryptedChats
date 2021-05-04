2287f64463cad61f73e328996e5c13174ad797d9
prakhar <prakhar.kamal2018@vitstudent.ac.in>
2021-05-04 14:05:22 +0530

    final

--- src/main/java/com/nisproject/cryptoaes/repositories/ChatRepo.java
+++ src/main/java/com/nisproject/cryptoaes/repositories/ChatRepo.java
@@ -10,6 +10,6 @@ import com.nisproject.cryptoaes.models.ChatEntity;
 
 @Repository
 public interface ChatRepo extends JpaRepository<ChatEntity, Long> {
-	@Query(value = "select * from chats c where (c.sender_id = ?1 and c.recipient_id = ?2) or ((c.sender_id = ?2 and c.recipient_id = ?1)) order by timestamp", nativeQuery = true)
+	@Query(value = "select * from chats c where (c.sender_id = ?1 and c.recipient_id = ?2) or ((c.sender_id = ?2 and c.recipient_id = ?1)) order by timestamp asc", nativeQuery = true)
 	List<ChatEntity> findChatsBySenderAndRecipient(String userId1, String userId2);
 }
